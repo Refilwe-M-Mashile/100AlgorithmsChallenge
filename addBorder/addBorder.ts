4eb67b22f3f12d11a74d838d1db100e692ea8114
Refilwe M Mashile <Refilwe-M-Mashile@users.noreply.github.com>
2021-11-14 18:04:28 +0200

    Added solution

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,10 @@
 export function addBorder(picture: string[]): string[] {
-
+    picture =  picture.map((i)=>{
+        let elem = picture[i].split("").push("*");
+        return elem.unshift("*").join("");
+    });
+    picture.push("*".repeat(picture[0].length))
+    picture.unshift("*".repeat(picture[0].length))
 }
 
 // console.log(addBorder(["abc", "ded"]));
