2ea925db1e16acb702aec2b66c1f5a6105294704
Refilwe M Mashile <Refilwe-M-Mashile@users.noreply.github.com>
2021-11-18 20:47:32 +0200

    Completed: 5 of 100

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,11 @@
 export function adjacentElementsProduct(inputArray: number[]): number {
-
+    let max = 1, prod = 1;
+    for (let i = 0; i < inputArray.length - 1; i++) {
+        prod = inputArray[i] * inputArray[i + 1];
+        if (max < prod)
+            max = prod;
+    }
+    return max;
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
