d19309159c1e695106b4c40c7945a4133708461a
Refilwe M Mashile <Refilwe-M-Mashile@users.noreply.github.com>
2021-11-18 20:58:46 +0200

    Completed: 6 of 100

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,13 @@
 export function allLongestStrings(inputArray: string[]): string[] {
-
+    let max = 0;
+    let maxLen = inputArray.forEach((w)=>{
+        if(w.length > max)
+        max = w.length;
+    });
+    return inputArray.filter((w)=>{
+        if(w.length == max)
+        return w;
+    });
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
