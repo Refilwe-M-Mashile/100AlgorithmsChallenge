7de4e46844d2c32dc1ca5dfdb83007b2d229f9b0
Refilwe M Mashile <Refilwe-M-Mashile@users.noreply.github.com>
2021-11-18 22:33:00 +0200

    Completed: 8 of 100

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,15 @@
 export function almostIncreasingSequence(sequence: number[]): boolean {
+    let i = -1, removals = 0;
+    for (let n of sequence) {
+        if (n < i) removals++;
+        else
+            i = n;
 
+        if (removals > 1) return false
+    }
+
+    return true
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+console.log(almostIncreasingSequence([1, 3, 2, 1]))
+console.log(almostIncreasingSequence([1, 3, 2]))
