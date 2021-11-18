0f9437d40cec656cfbb3cb4f9e5da2d27df5f643
Refilwe M Mashile <Refilwe-M-Mashile@users.noreply.github.com>
2021-11-18 05:39:32 +0200

    renamed file

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++
@@ -1,8 +0,0 @@
-export function absoluteValuesSumMinimization(a: number[]): number {
-    return a[Math.floor(a.length/2.5)];
-}
-
-console.log(absoluteValuesSumMinimization([2, 4, 7]));
-console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
