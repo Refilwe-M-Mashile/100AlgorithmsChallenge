42049b9b69b877146cd0c60a9bd85c1da3c0dec5
Refilwe M Mashile <Refilwe-M-Mashile@users.noreply.github.com>
2021-11-19 14:02:41 +0200

    Completed: 11 of 100

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,13 @@
-export function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
-
+export function areEquallyStrong(
+    yourLeft: number,
+    yourRight: number,
+    friendsLeft: number,
+    friendsRight: number
+): boolean {
+    let friendsArm = [friendsLeft, friendsRight];
+    return yourLeft + yourRight == friendsLeft + friendsRight && friendsArm.includes(yourLeft) && friendsArm.includes(yourRight);
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10));
+console.log(areEquallyStrong(15, 10, 15, 10));
+console.log(areEquallyStrong(15, 10, 15, 9));
