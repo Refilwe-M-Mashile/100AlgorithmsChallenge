101f4a3ad9b2b2a33e38f814e90b2a2cc06e514d
Refilwe M Mashile <Refilwe-M-Mashile@users.noreply.github.com>
2021-11-19 13:50:39 +0200

    Completed: 10 of 100

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,12 @@
 export function alternatingSums(a: number[]): number[] {
-
+    let sums = [0, 0];
+    for (let i = 0; i < a.length; i++) {
+        if (i % 2 == 0) {
+            sums[0] += a[i];
+        } else
+            sums[1] += a[i];
+    }
+    return sums;
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
