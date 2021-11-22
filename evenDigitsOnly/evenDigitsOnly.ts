a08cf6398042e1884215601caea1961e7e7d31b5
Refilwe M Mashile <Refilwe-M-Mashile@users.noreply.github.com>
2021-11-22 04:18:12 +0200

    Completed: 46 of 100

--- evenDigitsOnly/evenDigitsOnly.ts
+++ evenDigitsOnly/evenDigitsOnly.ts
@@ -1,6 +1,11 @@
 export function evenDigitsOnly(n: number): boolean {
 
+   return n.toString().split("").every((v)=>{
+        parseInt(v)%2 == 0
+    });
+
 }
 
-// console.log(evenDigitsOnly(248622));
-// console.log(evenDigitsOnly(642386));
+console.log(evenDigitsOnly(248622));
+console.log(evenDigitsOnly(642386));
+
