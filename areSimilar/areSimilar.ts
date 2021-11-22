84b87def56bb7f7f5d3c3c578fa2f690dbce53ef
Refilwe M Mashile <Refilwe-M-Mashile@users.noreply.github.com>
2021-11-22 04:49:29 +0200

    Completed: 12 of 100

--- areSimilar/areSimilar.ts
+++ areSimilar/areSimilar.ts
@@ -1,7 +1,13 @@
 export function areSimilar(a: number[], b: number[]): boolean {
+    let count = 0;
+    for (let i in a) {
+        if (a[i] != b[i]) count++;
+    }
+
+    return count==0 || count == 2;
 
 }
 
-// console.log(areSimilar([1, 2, 3], [1, 2, 3]));
-// console.log(areSimilar([1, 2, 3], [2, 1, 3]));
-// console.log(areSimilar([1, 2, 2], [2, 1, 1]));
+console.log(areSimilar([1, 2, 3], [1, 2, 3]));
+console.log(areSimilar([1, 2, 3], [2, 1, 3]));
+console.log(areSimilar([1, 2, 2], [2, 1, 1]));
