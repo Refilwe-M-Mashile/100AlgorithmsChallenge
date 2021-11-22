9670150b28b83796a1ce4bf12ba6caafaefcac8e
Refilwe M Mashile <Refilwe-M-Mashile@users.noreply.github.com>
2021-11-22 04:21:27 +0200

    Completed: 8 of 100

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -4,9 +4,10 @@ export function alphabeticShift(inputString: string): string {
         let ascii = c.charCodeAt(0);
         if (ascii == 122) ascii = 96;
         if (ascii == 90) ascii = 65;
-        output+= String.fromCharCode(ascii+1);
+        output += String.fromCharCode(ascii + 1);
     }
     return output;
 }
 
 console.log(alphabeticShift('crazy'));
+console.log(alphabeticShift('abcxyz'));
