5dadd0cd052dfe9d1dd229148925eec2f2cbc8c1
Refilwe M Mashile <Refilwe-M-Mashile@users.noreply.github.com>
2021-11-22 04:30:49 +0200

    Completed: 99 of 100

--- sumOfTwo/sumOfTwo.ts
+++ sumOfTwo/sumOfTwo.ts
@@ -1,5 +1,9 @@
 export function sumOfTwo(a: number[], b: number[], v: number): boolean {
+    for (let n of a) {
+        if (b.includes(v - n)) return true;
+    }
 
+    return false;
 }
 
-// console.log(sumOfTwo([1, 2, 3], [10, 20, 30, 40], 42));
+console.log(sumOfTwo([1, 7, 3], [10, 20, 30, 40], 42));
