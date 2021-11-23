baf3df3d00639cef87a6ca0f252075a0c9c506a8
Refilwe M Mashile <Refilwe-M-Mashile@users.noreply.github.com>
2021-11-23 10:23:27 +0200

    Completed: 13 of 100

--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,18 @@
 export function arrayChange(inputArray: number[]): number {
-
+    let moves = 0;
+    for (let i = 0; i < inputArray.length - 1; i++) {
+        if (i < inputArray.length - 1) {
+            if (inputArray[i] == inputArray[i + 1]) {
+                inputArray[i + 1]++;
+                moves++;
+            }
+            if (inputArray[i] < inputArray[i + 1]) {
+                moves += (inputArray[i + 1] - inputArray[i]) + 1;
+                inputArray[i + 1] += (inputArray[i + 1] - inputArray[i]) + 1;
+            }
+        }
+    }
+    return moves;
 }
 
-// console.log(arrayChange([1, 1, 1]));
+console.log(arrayChange([1, 2, 1]));
