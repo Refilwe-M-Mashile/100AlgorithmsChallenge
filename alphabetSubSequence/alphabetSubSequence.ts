b244554b0edc5f64bb2cdc58ac563846804f9f31
Refilwe M Mashile <Refilwe-M-Mashile@users.noreply.github.com>
2021-11-23 10:07:35 +0200

    Completed: 7 of 100

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,5 +1,13 @@
 export function alphabetSubsequence(s: string): boolean {
-
+    s = s.toLowerCase();
+    let ascii = s.charCodeAt(0);
+    let newS = [...new Set(s)];
+    if(newS.length != s.length) return false;
+    for (let c of newS) {
+        if(c.charCodeAt(0) < ascii) return false;
+        ascii = c.charCodeAt(0) ;
+    }
+    return true;
 }
 
 console.log(alphabetSubsequence('zab'))
