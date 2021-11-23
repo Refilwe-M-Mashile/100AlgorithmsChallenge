0a0d50206a55464f1a70aa482d1f6ebc9fa800b0
Refilwe M Mashile <Refilwe-M-Mashile@users.noreply.github.com>
2021-11-23 07:10:01 +0200

    Completed: 49 of 100

--- factorializeANumber/factorializeANumber.ts
+++ factorializeANumber/factorializeANumber.ts
@@ -1,6 +1,6 @@
 export function factorializeANumber(num: number): number {
-
+    return (num <= 2) ? num : num * factorializeANumber(num - 1);
 }
 
-// console.log(factorializeANumber(5));
-// console.log(factorializeANumber(10));
+console.log(factorializeANumber(5));
+console.log(factorializeANumber(10));
